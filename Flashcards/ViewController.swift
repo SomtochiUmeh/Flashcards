7c3ffb8aab44b2d6f67adaebf1ec6cea0a352f2e
Somtochi Umeh <74836691+SomtochiUmeh@users.noreply.github.com>
2022-03-01 21:59:02 +0900

    add buttons

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
     @IBOutlet weak var card: UIView!
+    @IBOutlet weak var btnOne: UIButton!
+    @IBOutlet weak var btnTwo: UIButton!
+    @IBOutlet weak var btnThree: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,6 +30,24 @@ class ViewController: UIViewController {
         // clip
         frontLabel.clipsToBounds = true
         backLabel.clipsToBounds = true
+        
+        // rounded button corner
+        btnOne.layer.cornerRadius = 25
+        btnOne.clipsToBounds = true
+        btnTwo.layer.cornerRadius = 25
+        btnTwo.clipsToBounds = true
+        btnThree.layer.cornerRadius = 25
+        btnThree.clipsToBounds = true
+        
+        // button width
+        btnOne.layer.borderWidth = 3.0
+        btnTwo.layer.borderWidth = 3.0
+        btnThree.layer.borderWidth = 3.0
+        
+        // button width color
+        btnOne.layer.borderColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        btnTwo.layer.borderColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        btnThree.layer.borderColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
@@ -37,5 +58,14 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func tapOptn1(_ sender: Any) {
+        btnOne.isHidden = true
+    }
+    @IBAction func tapOptn2(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+    @IBAction func tapOptn3(_ sender: Any) {
+        btnThree.isHidden = true
+    }
 }
 
